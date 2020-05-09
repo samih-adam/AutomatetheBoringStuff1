fd7bc2cc55aeda69880a23b3161d94e3243ad7d3
Samih Abdeljabbar <abdels6375@gmail.com>
2020-05-09 05:27:25 -0500

    first project that I did in python

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+# this program says hello and ask for my name
+
+print("Hello World")
+print("What is your name?")
+
+myName = input()
+print("it is great to meet you, " + myName) # asking there name
+
+print("the length of your name is:")
+
+print(len(myName))
+
+print("What is your age? " + myName) # asking for there age
+
+myAge = input()
+
+print("You will be " + str(int (myAge) + 1), " in a year!")
+
+
+
+
+
+
 
